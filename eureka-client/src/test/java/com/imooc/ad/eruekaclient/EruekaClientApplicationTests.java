336442e5f95d3d55b1e6ba56e921e0c952d7ff7c
caisong <826816058@qq.com>
2020-09-23 22:36:12 +0800

    first commit

---
+++ eureka-client/src/test/java/com/imooc/ad/eruekaclient/EruekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.ad.eruekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EruekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
